ce17ff4e9ecd3aff0c4c8668a1f30f0e77260c74
Taylor Corrello <tnc5484@rit.edu>
2018-11-22 18:21:27 -0600

    solved our first puzzle

--- solver.py
+++ solver.py
@@ -36,11 +36,25 @@ def findInRow(rnum, target, board):
             return True
     return False
 
+def removeFrompotentialInRow(rnum, value, board):
+    for colnum, col in enumerate(board[rnum]):
+        while value in col.potential:
+            col.potential.remove(value)
+        if len(col.potential) == 1:
+            fillInValue(rnum, colnum, col.potential[0], board)
+
 def findInCol(cnum, target, board):
     for i, row in enumerate(board):
         if board[i][cnum].value == target:
             return True
     return False
+
+def removeFromPotentialInCol(cnum, value, board):
+    for rownum, row in enumerate(board):
+        while value in board[rownum][cnum].potential:
+            board[rownum][cnum].potential.remove(value)
+        if len(board[rownum][cnum].potential) == 1:
+            fillInValue(rownum, cnum, board[rownum][cnum].potential[0], board)
 
 def findInSquare(x,y,target,board):
     intDivX = x//3
@@ -57,8 +71,52 @@ def findInSquare(x,y,target,board):
                 return True
     return False
 
+def removeFromPotentialInSquare(x, y, value, board):
+    intDivX = x//3
+    intDivY = y//3
+
+    minX = 3*intDivX
+    minY = 3*intDivY
+    maxX = 3*(intDivX+1)
+    maxY = 3*(intDivY+1)
+
+    for i in range(minX,maxX):
+        for j in range(minY,maxY):
+            while value in board[i][j].potential:
+                board[i][j].potential.remove(value)
+            if len(board[i][j].potential) == 1:
+                fillInValue(i, j, board[i][j].potential[0], board)
+
+def checkPossibilities(x, y, board):
+    if not (board[x][y].value == 0):
+        return
+    for val in range(1, size+1):
+        if not (findInRow(x, val, board) or findInCol(y, val, board) or findInSquare(x, y, val, board)):
+            board[x][y].potential.append(val)
+            print("Adding value to potential")
+            print(x)
+            print(y)
+            print(val)
+    if len(board[x][y].potential) == 1:
+        fillInValue(x, y, board[x][y].potential[0], board)
+
+def fillInValue(x, y, value, board):
+    removeFrompotentialInRow(x, value, board)
+    removeFromPotentialInCol(y, value, board)
+    removeFromPotentialInSquare(x, y, value, board)
+
+    #remove all potential
+    board[x][y].potential = []
+
+    board[x][y].value = value
+
+
 def solve(board):
-    print("todo")
+    complete = False
+    for x in range(0,size):
+        for y in range(0,size):
+            checkPossibilities(x, y, board)
+    printBoard(board)
 
 #loads a sudoku puzzle and prints the board
 puzzle = initBoard()
@@ -94,3 +152,6 @@ loadValue(7,7,5,puzzle)
 loadValue(8,3,9,puzzle)
 loadValue(8,8,6,puzzle)
 printBoard(puzzle)
+solve(puzzle)
+print(len(puzzle[0][1].potential))
+print(puzzle[0][1].potential)
